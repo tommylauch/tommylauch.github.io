e5a9cad875a40438407b3fa72484c860c2785b5f
tommylauch <ch.lau@lmu.de>
2025-01-20 03:21:43 -0600

    clean ups

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -1,7 +1,7 @@
 import { statSync } from "fs";
 
 export function remarkModifiedTime() {
-  return function (tree, file) {
+  return function (file) {
     const filepath = file.history[0];
     const result = statSync(filepath);
     file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
