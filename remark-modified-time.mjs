b03730bef1735c68493122c019379dc970bad086
tommylauch <ch.lau@lmu.de>
2025-01-20 11:51:06 -0600

    Revert "clean ups"
    This reverts commit e5a9cad875a40438407b3fa72484c860c2785b5f.

--- remark-modified-time.mjs
+++ remark-modified-time.mjs
@@ -1,7 +1,7 @@
 import { statSync } from "fs";
 
 export function remarkModifiedTime() {
-  return function (file) {
+  return function (tree, file) {
     const filepath = file.history[0];
     const result = statSync(filepath);
     file.data.astro.frontmatter.lastModified = result.mtime.toISOString();
